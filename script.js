e2daf7205a6bb64797c48e8f1fbd265b0534455c
Artem <artem7732@gmail.com>
2022-10-15 22:19:34 +0300

    js

--- script.js
+++ script.js
@@ -260,6 +260,11 @@
 // для ru - arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
 // для en - arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
 
+//let ru = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+// let en = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+// let lang = en;
+// console.log(lang);
+
 // Пример 3 
 // Напишите код, который предлагает пользователю ввести целое 
 // число. Нужно вывести на экран сколько в этом числе цифр, 
@@ -267,13 +272,28 @@
 // "Число " + num + " однозначное положительное". 
 // Достаточно будет определить, является ли число однозначным, 
 // двузначным или трехзначным и более.
-// Пример 4
 
+// Пример 4
 // Создаются переменные red и yellow для красного и 
 // жёлтого сигналов светофора соответственно.
 // В том случае, если переменным red или yellow присвоены 
 // значения "нет", горит зелёный сигнал светофора и 
 // выводиться сообщение, разрешающее переходить дорогу.
+//if (red == true) {
+//    console.log("стой");
+//} else {
+//    console.log("едь");
+//}
+//if (yellow == true) {
+//    console.log("приготовиться");
+//}
+//if (green == true) {
+//    console.log("true");
+//}
+
+
+
+
 
 // Циклы
 // Пример 5
@@ -293,7 +313,11 @@
 //}
 // Пример 7
 // Выведите столбец четных чисел в промежутке от 0 до 16.
-
+//for (let i = 0; i <= 16; i++) {
+//   if (i % 2 == 0) {
+//       console.log(i);
+//  }
+//}
 // Пример 8 
 // С помощью цикла найдите сумму чисел arr = [2,5,8,3,6]
 //let result = 1;
@@ -313,14 +337,14 @@
 // Выписать в комментариях. Вам необходимо найти все переменные,
 // функции, аргументы и параметры.
 // Запишите имена в соответсвующие пункты
-// Переменные:
-// Функции:
-// Встроенные функции:
-// Аргументы:
-// Параметры:
+// Переменные:years,myDog,guests
+// Функции:makeTea,dogYears
+// Встроенные функции:'Rex' 4
+// Аргументы:dogname,age,cups,tea
+// Параметры: 
 // function dogYears(dogname, age){
 //     let years = age * 2;
-//     console.log(dogName + ' is ' + years + ' years old');
+//     console.log(dogName + ' is ' + years + ' years old'); 
 // }
 // let myDog = 'Rex';
 // dogYears(myDog, 4);
@@ -349,7 +373,8 @@
 //let txt = 'я люблю javascript'
 //console.log(txt.substr(2));
 // console.log(txt.substring(2));
-//console.log(txt.slice(1));
+// console.log(txt.slice(1));
+
 
 // Дана строка 'я люблю JS!'. Найдите позицию 
 // подстроки 'люблю'.
@@ -365,6 +390,7 @@
 // троеточие '...'. В противном случае в переменную 
 // result запишем содержимое переменной txt.
 
+
 // Для решения задач данного блока вам понадобятся следующие 
 // методы: replace.
 // Дана строка 'Я-люблю-JS!'. 
@@ -372,16 +398,177 @@
 // поиска и замены.
 
 //let txt = 'Я-люблю-JS!';
-//console.log(txt.replace(/-/i, '!'))
+//console.log(txt.replace(/-/i, '!'));
 
 // Дана строка 'я люблю JS'. С помощью метода split 
 // запишите каждое слово этой строки в отдельный элемент 
 // массива.
+
 //let txt = 'я люблю JS';
 //console.log(txt.split(","));
 
 // Дана строка 'привет мир'. С помощью метода split 
 // запишите каждый символ этой строки в отдельный элемент 
 // массива.
-let txt = 'привет мир';
-console.log(txt.split(""));
+
+//let txt = 'привет мир';
+//console.log(txt.split(""));
+
+//'use strict';
+// Используя литерал объекта создайте пустой объект cat.
+// Добавьте свойство name со значением "Roxy".
+// Добавьте свойство lags со значением 4.
+// Добавьте свойство color со значением "grey".
+// Добавьте свойство full name со значением "Roxy, красивая кошка".
+// Выведите full name через console.log().
+// let cat = {};
+// cat.name = "Roxy";
+// cat.lags = 4;
+// cat.color = "grey";
+// cat["full name"] = "Roxy, красивая кошка";
+// console.log(cat["full name"]);
+
+// Напишите функцию-конструктор Person, которая принимает два параметра: 
+// name (имя) и age (возраст).
+// С помощью оператора new создайте объект person_1, передающий в 
+// функцию-конструктор аргументы: имя "Andrey" и возраст 28.
+// Выведите свойства объекта person_1 через console.log().
+// Добавьте к шаблону объекта Person свойство specialty с помощью 
+// prototype. Значением свойства будет "менеджер".
+// Выведите свойство specialty объекта person_1 через console.log().
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+// let person_1 = new Person('Andrey', 28);
+// console.log(person_1.name);
+// console.log(person_1.age);
+// console.log(person_1);
+
+// Person.prototype.specialty = "менеджер";
+// console.log(person_1.specialty);
+
+// Используя литерал объекта создайте объект cat с тремя свойствами: 
+// name, legs и color.
+// С помощью Object.create() создайте объект kitten 
+// объектом-прототипом которого будет cat.
+// Выведите свойства объекта kitten через console.log().
+
+// let cat = {
+//     legs:4,
+//     name : 'Roxy',
+//     color: 'grey'
+// }
+// let kitten = Object.create(cat);
+// console.log(kitten.name);
+// console.log(kitten.legs);
+// console.log(kitten.color);
+
+// Реализуйте класс Worker (Работник), который будет иметь следующие 
+// свойства: name (имя), surname (фамилия), rate (ставка за день работы), 
+// days (количество отработанных дней). Также класс должен иметь метод 
+// getSalary(), который будет выводить зарплату работника. Зарплата - 
+// это произведение (умножение) ставки rate на количество отработанных 
+// дней days.
+
+// class worker{
+//     constructor(name, surname, rate,days){
+//         this.name = name;
+//         this.surname = surname;
+//         this.rate = rate;
+//         this.days = days;
+//     }
+//     getSalary() {
+//         return this.rate * this.days;
+//     }
+// }
+// worker = new worker('Andrey', 'Makeenko', 100, 21);
+// console.log(worker.name);
+// console.log(worker.surname);
+// console.log(worker.getSalary());
+
+
+//Домашнее задание
+
+//1 Создайте объект city1 (var city1 = {}), укажите у него свойства name 
+// (название города, строка) со значением «ГородN» и population 
+// (населенность города, число) со значением 10 млн.
+let city1 = {
+    name: "Ncity",
+    population: 1000000
+};
+// Создайте объект city2 через нотацию {name: "ГородM", population: 1e6}.
+let city2 = {
+    name: "cityM",
+    population: 1e6
+};
+// Создайте у объектов city1 и city2 методы getName(), которые вернут 
+// соответствующие названия городов.
+function getName() {
+    return this.name;
+}
+city1.getName = getName;
+city2.getName = getName;
+
+// Создайте методы exportStr() у каждого из объектов. Этот метод должен 
+// возвращать информацию о городе в формате 
+// «name=ГородN\npopulation=10000000\n». Для второго города будет строка 
+// со своими значениями. Примечание: можно обращаться к каждому свойству 
+// через цикл for/in, но методы объекта возвращать не нужно.
+function exportStr() {
+    return `name=${this.name}\npopulation=${this.population}\n`;
+}
+city1.exportStr = exportStr;
+city2.exportStr = exportStr;
+// Создайте глобальную функцию getObj(), которая возвращает this. 
+// А у каждого из объектов city1 или city2 метод getCity, который 
+// ссылается на getObj. Проверьте работу метода. Примечание: к объекту 
+// вызова можно обратиться через this..
+function getObj() {
+    return this;
+}
+
+function getCity() {
+    return getObj.apply(this).getName
+}
+city1.getCity = getCity;
+city2.getCity = getCity;
+console.log(city1.exportStr());
+console.log(city2.exportStr());
+console.log(city1.getCity());
+console.log(city2.getCity());
+//2 Создайте массив d1 с числовыми величинами 45,78,10,3.  
+// Добавьте в массив d1 еще одно число ( d1[7] = 100). 
+// Выведите в консоль весь массив и его элементы с индексами 6 и 7.
+let d1 = [45, 78, 10, 3];
+d1[7] = 100;
+console.log(d1);
+// Создайте массив d2 с числовыми величинами 45,78,10,3. 
+// Посчитайте в переменную sum2 сумму чисел в нем, при помощи цикла for.
+let d2 = [45, 78, 10, 3];
+let sum2 = 0;
+for (let i = 0; i < d2.length; i++) {
+    sum2 += d2[i];
+    console.log(sum2);
+}
+//почитай for/in !!!
+
+// Создайте массив d3 с числовыми величинами 45,78,10,3. 
+// Добавьте в массив d3 еще одно число (например, d[7] = 100) 
+// Посчитайте в переменную sum3 сумму чисел в нем, при помощи цикла for/in.
+let d3 = [45, 78, 10, 3];
+d3[7] = 100;
+let sum3 = 0;
+for (let i in d3) {
+    sum3 += d3[i];
+    console.log(sum3);
+
+}
+// Создайте массив d4 с числовыми величинами 45,78,10,3. 
+// Напишите функцию сортировки my(a,b), которая при вызове d4.sort(my) 
+// отсортирует элементы массива по убыванию чисел. Вызовите d4.sort(my)
+let d4 = [45, 78, 10, 3];
+d4.sort(function my(a, b) {
+    return b - a;
+})
+console.log(d4);
